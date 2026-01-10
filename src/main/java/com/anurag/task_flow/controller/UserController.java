c0e9ab870a843ffd8c40e14afebc63052c5d685a
Anurag <anuragyadav41682@gmail.com>
2026-01-10 23:22:08 +0530

    Added Controller to find user by ID

--- src/main/java/com/anurag/task_flow/controller/UserController.java
+++ src/main/java/com/anurag/task_flow/controller/UserController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -53,6 +54,12 @@ public class UserController {
 
     allUser.forEach(element -> response.add(mapToUserResponse(element)));
     return ResponseEntity.ok(response);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<UserResponse> getUserById(@PathVariable Long id) {
+    User user = userService.getUserById(id);
+    return ResponseEntity.ok(mapToUserResponse(user));
   }
 
 }
