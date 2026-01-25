06d2fbcf940222faa5abbb93dad46121e1b0cb85
Anurag <anuragyadav41682@gmail.com>
2026-01-25 18:27:42 +0530

    Updated and added bean in config files for authentication and authorization

--- src/main/java/com/anurag/task_flow/config/SecurityConfig.java
+++ src/main/java/com/anurag/task_flow/config/SecurityConfig.java
@@ -2,23 +2,60 @@ package com.anurag.task_flow.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.anurag.task_flow.security.CustomAccessDeniedHandler;
+import com.anurag.task_flow.security.CustomAuthEntryPoint;
+import com.anurag.task_flow.security.JwtAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
+  private final JwtAuthenticationFilter jwtAuthenticationFilter;
+  private final CustomAuthEntryPoint customAuthEntryPoint;
+  private final CustomAccessDeniedHandler customAccessDeniedHandler;
+
+  public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter, CustomAuthEntryPoint customAuthEntryPoint,
+      CustomAccessDeniedHandler customAccessDeniedHandler) {
+    this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    this.customAuthEntryPoint = customAuthEntryPoint;
+    this.customAccessDeniedHandler = customAccessDeniedHandler;
+  }
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
     http
-        .csrf(csrf -> csrf.disable()) // allow POST from Postman
-        .authorizeHttpRequests(auth -> auth
-            .anyRequest().permitAll() // allow all endpoints
-        );
+        .csrf(csrf -> csrf.disable())
+        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authorizeHttpRequests(auth -> auth.requestMatchers("/api/auth/**").permitAll()
+            .anyRequest().authenticated())
+        .exceptionHandling(
+            ex -> ex.authenticationEntryPoint(customAuthEntryPoint).accessDeniedHandler(customAccessDeniedHandler))
+        .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
     return http.build();
   }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+      throws Exception {
+    return authenticationConfiguration.getAuthenticationManager();
+  }
+
 }
