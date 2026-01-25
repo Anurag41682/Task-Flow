2686c0cfc078507a8054b46228252025746c6146
Anurag <anuragyadav41682@gmail.com>
2026-01-26 02:42:46 +0530

    Added Validation for setPasswordRequest dto

--- src/main/java/com/anurag/task_flow/dto/request/SetPasswordRequest.java
+++ src/main/java/com/anurag/task_flow/dto/request/SetPasswordRequest.java
@@ -1,8 +1,12 @@
 package com.anurag.task_flow.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class SetPasswordRequest {
+  @NotNull
+  @NotBlank
   String newPassword;
 }
