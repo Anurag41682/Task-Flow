0521b61c53e4e1798bc8087b91c286c2719da784
Anurag <anuragyadav41682@gmail.com>
2026-03-28 07:43:17 +0530

    Added PageRepsonse file

---
+++ src/main/java/com/anurag/task_flow/dto/response/PageResponse.java
@@ -0,0 +1,15 @@
+package com.anurag.task_flow.dto.response;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class PageResponse<T> {
+  private List<T> content;
+  private int totalPages;
+  private long totalElements;
+  private int pageNumber;
+}
