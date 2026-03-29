786c2c40c75d8aee8231c4fbf809c48545884db0
Anurag <anuragyadav41682@gmail.com>
2026-03-30 01:01:49 +0530

    Upadated from SMTP to HTTP to send mail

--- src/main/java/com/anurag/task_flow/service/impl/EmailServiceImpl.java
+++ src/main/java/com/anurag/task_flow/service/impl/EmailServiceImpl.java
@@ -1,8 +1,11 @@
 package com.anurag.task_flow.service.impl;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.anurag.task_flow.service.EmailService;
@@ -10,15 +13,33 @@ import com.anurag.task_flow.service.EmailService;
 @Service
 public class EmailServiceImpl implements EmailService {
 
-  @Autowired
-  private JavaMailSender mailSender;
+  @Value("${resend.api.key}")
+  private String resendApiKey;
 
   public void sendSetPasswordEmail(String toEmail, String link) {
-    SimpleMailMessage mailMessage = new SimpleMailMessage();
-    mailMessage.setTo(toEmail);
-    mailMessage.setSubject("Set Your Password");
-    mailMessage.setText("Clink the link to set the password:\n" + link);
-    mailSender.send(mailMessage);
-  }
+    try {
+      String body = """
+          {
+            "from": "noreply@agnomerf.store",
+            "to": "%s",
+            "subject": "Set Your Password",
+            "text": "Click the link to set the password:\\n%s"
+          }
+          """.formatted(toEmail, link);
 
+      HttpClient client = HttpClient.newHttpClient();
+      HttpRequest request = HttpRequest.newBuilder()
+          .uri(URI.create("https://api.resend.com/emails"))
+          .header("Authorization", "Bearer " + resendApiKey)
+          .header("Content-Type", "application/json")
+          .POST(HttpRequest.BodyPublishers.ofString(body))
+          .build();
+
+      HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+      System.out.println("Resend response: " + response.body());
+
+    } catch (Exception e) {
+      System.out.println("Email error: " + e.getMessage());
+    }
+  }
 }
